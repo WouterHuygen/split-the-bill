7cc865ecad9e41d91e798c4acd4e439c75798d0c
Unknown <weyns.arno@gmail.com>
2019-12-09 17:17:59 +0100

    Payment overview on group settle added

--- src/com/uantwerpen/GroupOverviewPanel.java
+++ src/com/uantwerpen/GroupOverviewPanel.java
@@ -2,6 +2,7 @@ package com.uantwerpen;
 
 import com.uantwerpen.Helpers.DbWriter;
 import com.uantwerpen.Objects.GroupMember;
+import com.uantwerpen.Objects.PaymentGroup;
 import com.uantwerpen.Objects.Transaction;
 
 import javax.swing.*;
@@ -17,6 +18,10 @@ public class GroupOverviewPanel {
     private JButton buttonAddTransaction;
     private JPanel panelTransactionHistory;
     private JPanel panelMemberBalances;
+    private JButton buttonSettleGroup;
+    private JButton buttonPaymentOverview;
+
+    private String settleText;
 
     public JPanel getGroupOverviewPanel() {
         return groupOverviewPanel;
@@ -28,8 +33,19 @@ public class GroupOverviewPanel {
 
     private ArrayList<String> transactionNames = new ArrayList<String>();
 
+    public ArrayList<GroupMember> memberList = new ArrayList<GroupMember>();
+    public ArrayList<GroupMember> negativeMembers = new ArrayList<GroupMember>();
+    public ArrayList<GroupMember> positiveMembers = new ArrayList<GroupMember>();
+
+
     public GroupOverviewPanel(){
         transactions = dbWriter.GetTransactionsByGroupId(PanelController.getInstance().getCurrentGroupId());
+        PaymentGroup currentPaymentGroup = dbWriter.GetGroupByGroupId(PanelController.getInstance().getCurrentGroupId());
+        if (currentPaymentGroup.isSettled()){
+            buttonAddTransaction.setVisible(false);
+            buttonManageGroup.setVisible(false);
+            buttonSettleGroup.setVisible(false);
+        }
 
         for (Transaction t: transactions) {
             transactionNames.add(t.getName());
@@ -49,6 +65,57 @@ public class GroupOverviewPanel {
                 PanelController.getInstance().makeGroupPanel();
             }
         });
+        buttonSettleGroup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int groupId = PanelController.getInstance().getCurrentGroupId();
+                dbWriter.SettleGroupByGroupId(groupId);
+                getPaymentOverview();
+                JOptionPane.showMessageDialog(null, "Group is being settled, the following balances are still open: " + "\n" + settleText);
+                PanelController.getInstance().makeMainPanel();
+            }
+        });
+        buttonPaymentOverview.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                getPaymentOverview();
+                JOptionPane.showMessageDialog(null, settleText);
+            }
+        });
+    }
+
+    public void getPaymentOverview(){
+        settleText = "";
+        negativeMembers.clear();
+        positiveMembers.clear();
+        memberList.clear();
+        
+        memberList = dbWriter.GetMembersByGroupId(PanelController.getInstance().getCurrentGroupId());
+        for (GroupMember gm :
+                memberList) {
+            if (gm.balance == 0.0) continue;
+            else if (gm.balance > 0) positiveMembers.add(gm);
+            else negativeMembers.add(gm);
+        }
+        //Collections.sort(positiveMembers, GroupMember::compareTo);
+        //System.out.println(positiveMembers);
+        for (int i = 0; i < negativeMembers.size(); i++) {
+            for (int j = 0; j < positiveMembers.size(); j++) {
+                GroupMember negativeMember = negativeMembers.get(i);
+                GroupMember positiveMember = positiveMembers.get(j);
+                if(positiveMember.balance == 0.0) continue;
+                else if (Math.abs(negativeMember.balance) > positiveMember.balance){
+                    negativeMember.balance += positiveMember.balance;
+                    settleText += (negativeMembers.get(i).name + " owes " + positiveMember.name + " €" + (double)Math.round(positiveMember.balance) + "\n");
+                    positiveMember.balance = 0.0;
+
+                }else if(Math.abs(negativeMember.balance) < positiveMember.balance){
+                    settleText += (negativeMember.name + " owes " + positiveMember.name + " €" + (double)Math.round(Math.abs(negativeMember.balance)) + "\n");
+                    negativeMember.balance = 0.0;
+                    positiveMember.balance += negativeMember.balance;
+                }
+            }
+        }
     }
 
     private void initTransactionList(){
@@ -91,6 +158,7 @@ public class GroupOverviewPanel {
                 }
             }
     };
+
 
 
 }
